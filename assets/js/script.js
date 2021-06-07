eaa8d80be7a1624ed0ebcda673bd2d0bb8c3d6d9
Ethan Harsh <ethan@ethanharsh.com>
2021-06-07 23:12:14 +0000

    prevent default/null url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //Search Bar Event Listener
 $('#search-button').click(() => {
+    event.preventDefault()
     let val = $('#search-bar').val();
     url = 'https://api.openbrewerydb.org/breweries/search?query=' + val;
     fetch(url)
@@ -8,6 +9,7 @@ $('#search-button').click(() => {
 })
 
 function renderSearch(data) {
+
     $('#search-results').empty();
     $('#search-results').removeClass('hide');
     $('#search-form').removeClass('m-0-bottom');
@@ -16,11 +18,17 @@ function renderSearch(data) {
         let result = 'result-' + i;
         let content = 'content-' + i;
         let phone = formatPhoneNumber(el.phone)
+        let url = el.website_url;
+        let hrefURL = el.website_url;
+        if(url === null) {
+            url = 'No Website Listed';
+            hrefURL = '#';
+        }
         if(phone === null) {
             phone = 'No Phone Number Listed'
         }
         let contentHref = '#content-' + i;
-        let item = '<li class="accordion-item" data-accordion-item><a href="' + contentHref + '" role="tab" class="accordion-title" id="' + result + '">'+ el.name + '</a><div class="accordion-content" role="tabpanel" data-tab-content id="' + content + '"><h3 class="border-bottom-black">' + el.brewery_type.toUpperCase() + '</h3><h4>' + el.street + '</h4><h4 class="border-bottom-black">' + el.city + ', ' + el.state + '</h4><h5>' + phone + '</h5><a href="' + el.website_url + '" target="_blank">' + el.website_url + '</a></div></li>'
+        let item = '<li class="accordion-item" data-accordion-item><a href="' + contentHref + '" role="tab" class="accordion-title" id="' + result + '">'+ el.name + '</a><div class="accordion-content" role="tabpanel" data-tab-content id="' + content + '"><h3 class="border-bottom-black">' + el.brewery_type.toUpperCase() + '</h3><h4>' + el.street + '</h4><h4 class="border-bottom-black">' + el.city + ', ' + el.state + '</h4><h5>' + phone + '</h5><a href="' + hrefURL + '" target="_blank">' + url + '</a></div></li>'
         $('#search-results').append(item);
         i++;
     })
