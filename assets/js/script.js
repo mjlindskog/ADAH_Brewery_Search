1e2feb243e2f08e609f97696f95801d0a838dbc1
LaurenR01 <78819957+LaurenR01@users.noreply.github.com>
2021-06-07 21:20:28 -0400

    fixed api pull

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
 // Weather App Functionality//
-
 // setting the API key
+// $(document).ready(function() {
+    // $(“.currentDate”).text(currentDate);
+
 const api = {
     key: '5eb289bb53abf02581c3494d598b36af',
     base: 'http://api.openweathermap.org/data/2.5/'
 }
-
-// capturing the information from the button
-var zipcode = document.querySelector('#zip-code').value;
-var button = document.querySelector('.submitBtn');
-button.addEventListener('click', saveLocation);
-
-var saveLocation = () =>{
-    localStorage.setItem('location', zipcode);
+// capturing zip code
+var button = $(".submitBtn");
+$(button).on('click', function(event){
+    zipcode = $("#zip-code").val();
     console.log(zipcode);
-}
+    getWeather(zipcode);
+});
 
+// grabbing weather info from the API
 function getWeather(zipcode) {
-    fetch (`${api.base}weather?zip=${zipcode}&units=imperial&APPID=${api.key}`)
-    .then(current => {
-        return current.json();
-    }).then(showWeather);
-
-    function showWeather (weather) {
+    fetch (`${api.base}weather?zip=${zipcode}&units=imperial&appid=${api.key}`)
+    .then (current => {
+        return current.json();})
+    .then(showWeather);
+}
+// show weather on screen
+function showWeather (current) {
         console.log(current);
     }
-}
+// })
