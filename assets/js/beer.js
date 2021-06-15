cbbd8000c0138db30a67de25f80fe83f81e33972
Ethan Harsh <ethan@ethanharsh.com>
2021-06-15 01:29:32 +0000

    location bug

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -28,12 +28,65 @@ $('#brewery-city').text(brewery.state);
 $('#brewery-phone').text(phone);
 $('#brewery-url').text(url);
 
-//Weather
 const api = {
     key: '5eb289bb53abf02581c3494d598b36af',
     base: 'https://api.openweathermap.org/data/2.5/'
 }
-getWeather();
+
+if(latitude != null){
+    //Weather
+
+    getWeather();
+    // Inserting the map 
+    var mymap = L.map('mapid').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2xvbmd5NyIsImEiOiJja3BxNHpzdzcwOHJzMnNydjdpeDI2dWIxIn0.5FFHiq6sQIPXBxUYB0o_qQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+
+    // creating an "I am here" circle
+    L.marker([latitude, longitude], 500, {}).addTo(mymap).bindPopup(brewery.name);
+
+    // possible parameters
+    var eventTime = "&localStartDateTime=";
+    var eventCity = "&city=";
+    var eventLocation = "&latlong=";
+    var locationPreferred = "&geoPoint=";
+    var eventRadius = "&radius=";
+    var eventDistance = "&unit=5";
+
+    $.ajax({
+        type:"GET",
+        url: apiURL,
+        async:true,
+        dataType: "json",
+        success: function(json) {
+            //console.log('json')
+        // console.log(json)
+            //console.log('events');
+            if(json._embedded == null) {
+                //console.log('embeded null')
+                $('#event-box').empty();
+                $('#event-box').text('No events found 🥲')
+            } else {
+                renderEventCards(json._embedded.events);
+            }
+        },
+        error: function(xhr, status, err) {
+                    // This time, we do not end up here!
+                }
+    });
+} else {
+    $('#mapid').addClass('hide');
+    $('#weather').addClass('hide');
+    $('#event-box').text("No events found 🥲")
+}
+
 
 function getWeather() {
     fetch (`${api.base}weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${api.key}`)
@@ -47,50 +100,7 @@ function getWeather() {
 }
 
 
-// Inserting the map 
-var mymap = L.map('mapid').setView([latitude, longitude], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2xvbmd5NyIsImEiOiJja3BxNHpzdzcwOHJzMnNydjdpeDI2dWIxIn0.5FFHiq6sQIPXBxUYB0o_qQ', {
-attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-maxZoom: 18,
-id: 'mapbox/streets-v11',
-tileSize: 512,
-zoomOffset: -1,
-accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
-
-// creating an "I am here" circle
-L.marker([latitude, longitude], 500, {}).addTo(mymap).bindPopup(brewery.name);
-
-// possible parameters
-var eventTime = "&localStartDateTime=";
-var eventCity = "&city=";
-var eventLocation = "&latlong=";
-var locationPreferred = "&geoPoint=";
-var eventRadius = "&radius=";
-var eventDistance = "&unit=5";
-
-$.ajax({
-    type:"GET",
-    url: apiURL,
-    async:true,
-    dataType: "json",
-    success: function(json) {
-        //console.log('json')
-       // console.log(json)
-        //console.log('events');
-        if(json._embedded == null) {
-            //console.log('embeded null')
-            $('#event-box').empty();
-            $('#event-box').text('No events found 🥲')
-        } else {
-            renderEventCards(json._embedded.events);
-        }
-    },
-    error: function(xhr, status, err) {
-                // This time, we do not end up here!
-             }
-});
 
 
 function getData(){
